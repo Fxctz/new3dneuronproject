188fa3ea69769686bad459536980c19b5f0eb5b0
Fxctz <138025362+Fxctz@users.noreply.github.com>
2023-08-30 00:29:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function MOVEN() {
+    window.location.href = "N.html";
+}
+function MOVES() {
+    window.location.href = "S.html";
+}
+function MOVESP() {
+    window.location.href = "SP.html";
+}
